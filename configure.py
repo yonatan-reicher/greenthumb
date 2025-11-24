3104db62eff8cd80f26c85412a427cc984c31bd3
Jonathan Reicher <yony252525@gmail.com>
2025-11-24 16:40:56 +0200

    Migrate to python3

--- configure.py
+++ configure.py
@@ -1,3 +1,5 @@
+# Original python 2 code:
+"""
 import commands
 
 # Create path.rtk
@@ -7,3 +9,13 @@ f.write("#lang racket\n")
 f.write("(provide (all-defined-out))\n")
 f.write("(define srcpath \"" + abs_path + "\")\n")
 print "path.rkt is created."
+"""
+
+from pathlib import Path
+
+abs_path = str(Path('.').absolute())
+with open("path.rkt", "w") as f:
+    f.write("#lang racket\n")
+    f.write("(provide (all-defined-out))\n")
+    f.write("(define srcpath \"" + abs_path + "\")\n")
+    print("path.rkt is created.")
